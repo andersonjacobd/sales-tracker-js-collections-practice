0fb63816fef80d7080f86a72084bd3715f7599b5
Jacob <11730883+andersonjacobd@users.noreply.github.com>
2021-04-16 13:47:53 -0700

    Collections Practice with Chart.js
    - using Array.of (Array literal more used and valid just practicing)
    - spread operator practice
    - forEach practice
    - Array.fill, Array.find practice

--- js/page.js
+++ js/page.js
@@ -1,15 +1,46 @@
 // Charts
 let ctx = document.getElementById("monthlySales").getContext("2d");
 let pieCtx = document.getElementById("deptSales").getContext("2d");
+let yearlyLabel = document.getElementById("yearlyTotal");
+
+let monthlySales = Array.of(1200, 9000, 3000);
+let monthlyLabels = Array.of("Oct", "Nov", "Dec");
+
+let deptSales = Array.of(12, 9, 3);
+let deptLabels = Array.of("Hiking", "Running", "Hunting");
+let yearlyTotal = 0;
+
+function addYearlyTotal(x) {
+    yearlyTotal =+ x;
+}
+
+monthlySales.forEach(addYearlyTotal);
+
+let octNums = Array.of(1200, 1000, 9000);
+let novNums = Array.of(1100, 2000, 9000);
+let decNums = Array.of(4800, 1000, 5000);
+//let total = Array.of(addYearlyTotal(...octNums), addYearlyTotal(...novNums), addYearlyTotal(...decNums));
+
+yearlyLabel.innerHTML = "$" + yearlyTotal;
+
+function findOver1000(){
+    let firstThousand = monthlySales.find(s => s > 1000);
+    alert(firstThousand);
+}
+
+function resetNum(){
+	monthlySales.fill(0);
+	monthlySalesChart.update();
+}
 
 // Bar
 var monthlySalesChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Oct', 'Nov', 'Dec'],
+        labels: monthlyLabels,
         datasets: [{
             label: '# of Sales',
-            data: [14, 9, 3],
+            data: monthlySales,
             backgroundColor: [
                 'rgba(238, 184, 104, 1)',
                 'rgba(75, 166, 223, 1)',
@@ -33,10 +64,10 @@ var monthlySalesChart = new Chart(ctx, {
 var deptSalesChart = new Chart(pieCtx, {
     type: 'pie',
     data: {
-        labels: ["Hiking", "Running", "Hunting"],
+        labels: deptLabels,
         datasets: [{
             label: '# of Sales',
-            data: [12, 9, 3],
+            data: deptSales,
             backgroundColor: [
                 'rgba(238, 184, 104, 1)',
                 'rgba(75, 166, 223, 1)',
